17036006366cfd71c8cd6a3dd63e42e4b01fc2ed
natteynikhil <natteynikhil41@gmail.com>
2024-02-21 21:07:36 +0530

    changing images paths as per the video

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -49,7 +49,7 @@ const Auth = () => {
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repear bg-center bg-fixed bg-cover">
             <div className="bg-black w-full h-full lg:bg-opacity-50">
                 <nav className="px-12 py-5">
-                    <img src="public/images/logo.png" alt="Logo" className="h-12"/>
+                    <img src="/images/logo.png" alt="Logo" className="h-12"/>
                 </nav>
                 <div className="flex justify-center">
                     {/* <h1 className="text-white px-12 py-10"> Hello </h1> */}
